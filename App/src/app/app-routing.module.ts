78be77427f1a0e4378fb30af16641b49923684d8
Dimcon <daimonsewell@gmail.com>
2021-11-28 12:55:37 +0200

    Auth, SSL, some minor refactors. Working.

--- App/src/app/app-routing.module.ts
+++ App/src/app/app-routing.module.ts
@@ -9,23 +9,23 @@ const routes: Routes = [
   },
   {
     path: 'register',
-    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+    loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterPageModule)
   },
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
   },
   {
     path: 'dashboard',
-    loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardPageModule)
+    loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardPageModule)
   },
   {
     path: 'nfts',
-    loadChildren: () => import('./nfts/nfts.module').then( m => m.NftsPageModule)
+    loadChildren: () => import('./pages/nfts/nfts.module').then(m => m.NftsPageModule)
   },
   {
     path: 'gits',
-    loadChildren: () => import('./gits/gits.module').then( m => m.GitsPageModule)
+    loadChildren: () => import('./pages/gits/gits.module').then(m => m.GitsPageModule)
   },
 
 ];
--- App/src/app/services/auth.service.ts
+++ App/src/app/services/auth.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
-import { HttpServiceService } from './http-service.service';
+import {Injectable} from '@angular/core';
+import {HttpServiceService} from './http-service.service';
 
-import { environment } from 'src/environments/environment';
-import { UtilityServiceService } from './utility-service.service';
+import {environment} from 'src/environments/environment';
+import {UtilityServiceService} from './utility-service.service';
 import * as moment from 'moment';
 
-const baseUrl = environment.apiUrl
+const baseUrl = environment.apiUrl;
 
 @Injectable({
   providedIn: 'root'
@@ -15,59 +15,60 @@ export class AuthService {
   constructor(
     private httpService: HttpServiceService,
     private utilityService: UtilityServiceService
-  ) { }
+  ) {
+  }
 
-  async login(userid: string, pass: string){
-    const result = await this.httpService.post(`${baseUrl}/auth/authenticate`,{
+  async login(userid: string, pass: string) {
+    const result = await this.httpService.post(`${baseUrl}/auth/authenticate`, {
       username: userid,
-      passwor: pass
-    })
-    if(result.success){
-      this.httpService.setAuthToken(result.token)
+      password: pass
+    });
+    if (result.success) {
+      this.httpService.setAuthToken(result.token);
       this.utilityService.user = result.user;
-      const expiresAt = moment().add(result.expiresIn,'second');
+      const expiresAt = moment().add(result.expiresIn, 'second');
       localStorage.setItem('jwt_token', result.token);
       localStorage.setItem('user', JSON.stringify(result.user));
-      localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()));
-    }else{
-      throw "Credentials are bad."
+      localStorage.setItem('expires_at', JSON.stringify(expiresAt.valueOf()));
+    } else {
+      throw 'Credentials are bad.';
     }
   }
 
-  getAuthToken(){
-    return localStorage.getItem("jwt_token");
+  getAuthToken() {
+    return localStorage.getItem('jwt_token');
   }
 
-  logout(){
-    localStorage.removeItem("jwt_token");
-    localStorage.removeItem("user");
-    localStorage.removeItem("expires_at");
+  logout() {
+    localStorage.removeItem('jwt_token');
+    localStorage.removeItem('user');
+    localStorage.removeItem('expires_at');
   }
 
-  async register(username: string, email: string, pass: string){
-    const result = await this.httpService.post(`${baseUrl}/auth/register`,{
+  async register(username: string, email: string, pass: string) {
+    const result = await this.httpService.post(`${baseUrl}/auth/register`, {
       name: username,
-      email: email,
+      email,
       username: email,
       password: pass,
       role: 'user'
-    })
+    });
     if (result.success) {
-      await this.login(email, pass)
+      await this.login(email, pass);
     } else {
-      throw "Credentials are bad."
+      throw new Error('Credentials are bad.');
     }
   }
 
   public isLoggedIn() {
-    const userjs = localStorage.getItem('user')
-    this.httpService.setAuthToken(this.getAuthToken() || '')
-    this.utilityService.user = JSON.parse(userjs || '{}')
+    const userjs = localStorage.getItem('user');
+    this.httpService.setAuthToken(this.getAuthToken() || '');
+    this.utilityService.user = JSON.parse(userjs || '{}');
     return moment().isBefore(this.getExpiration());
   }
 
   getExpiration() {
-    const expiration = localStorage.getItem("expires_at");
+    const expiration = localStorage.getItem('expires_at');
     const expiresAt = JSON.parse(expiration || '0');
     return moment(expiresAt);
   }
